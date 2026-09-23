5e88b46ba0a65079b8bd2274627f5551d49adc8a
sourovadikari <mr.sourovadikari@gmail.com>
2026-09-23 14:42:38 +0000

    Refine player menus and controls

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Space_Grotesk, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 import "../components/media-player/media-player.css";
 import "../components/media-player/player-tuning.css";
+import "../components/media-player/player-final.css";
 
 const display = Space_Grotesk({
   variable: "--font-display",
